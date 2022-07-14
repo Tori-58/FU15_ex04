011c49e61965c8626c6742942d38e710e249c0f4
Tori-58 <s1290058@u-aizu.ac.jp>
2022-07-14 22:28:24 +0900

    Victory message added

--- hot.c
+++ hot.c
@@ -7,9 +7,14 @@ int main()
 {
 	int countH = 0;
 	int countT = 0;
+	char name[20];
 	
 	srand((unsigned int)time(NULL));
 	
+	printf("Who are you?\n");
+	scanf("%s",name);
+	printf("Hello, %s!\n",name);
+
 	printf("Tossing a coin...\n");
 	
 	for(int i = 1; i <= 3; i++)
@@ -28,4 +33,6 @@ int main()
 	}
 	
 	printf("Heads: %d, Tails: %d\n",countH,countT);
+	if(countH > countT) printf("You won!\n");
+	else printf("You lost\n");
 }
